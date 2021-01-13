420c47711cd6f30d6acb31e2cff1921d1867385e
Dirkhoogland <99063413@mydavinci.nl>
2021-01-13 09:54:40 +0100

    lab 1 af

--- meerknoppen2.js
+++ meerknoppen2.js
@@ -1,24 +1,36 @@
 var container = document.getElementById('container');
 
-var buttons = document.getElementsByClassName("buttons");
+var aantalButtons = prompt("hoeveel buttons");
+document.body.style.backgroundColor = "white"
+kleuren = ["green","red","purple","blue","black"];	
 
-var aantalbuttons = prompt("hoeveel buttons");
 
-var kleuren = ["green","red","purple","blue","black"];
-
-for (p =1; p<=aantalbuttons; p++){
+for (p =1; p<=aantalButtons; p++){
 	var maakButton = document.createElement("button");
 	var Buttonnaam = document.createTextNode(p);
 	maakButton.appendChild(Buttonnaam);
-	maakButton.classList = "buttons";
-	maakButton.id = "button" + p;
 	maakButton.style.backgroundColor = kleuren[0];
 	container.appendChild(maakButton);
 
-
+maakButton.onclick = function(){
+buttonColor(this);
 
 };
 
-maakButton.onclick = function(kleuren){
-maakButton.style.backgroundColor = kleuren[1];
 };
+function buttonColor(knop){
+if (knop.style.backgroundColor == "black") {
+	knop.style.display = "none"
+}
+
+	var kleur = kleuren.value;
+	var naamkleur = kleuren.indexOf(knop.style.backgroundColor);
+
+	console.dir(knop.style.backgroundColor);
+knop.style.backgroundColor = kleuren[naamkleur+1];
+
+
+
+}
+
+
